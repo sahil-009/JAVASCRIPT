25b4c68c64dad984fe39e5c0eddb04bc7624b6fd
Sahil Mund <130903382+sahil-009@users.noreply.github.com>
2023-12-23 12:40:32 +0530

    Add files via upload

---
+++ control-flow.js
@@ -0,0 +1,31 @@
+// if 
+
+// switch
+
+// && ||
+// falsy values
+// false , 0, -0, BigInt 0n, " ", null, undefined, NaN
+
+// truthy values
+// " 0", 'false' , " ", [], {}, function(){}
+
+
+
+
+
+//Nullish Colescing Operator (??): null undefined
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? null
+
+// console.log(val1);  //10
+// console.log(val1);  //10
+
+//terniary operator
+
+//condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
